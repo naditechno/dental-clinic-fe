2ad2b124d908985526e54dd3555141228257bceb
akmlrafly <akmalrafly875@gmail.com>
2025-06-27 16:00:28 +0700

    - fetch data

--- src/providers/redux.tsx
+++ src/providers/redux.tsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 function ReduxProvider({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <Provider store={store}>{children}</Provider>;
+      <Provider store={store}>{children}</Provider>
     </SessionProvider>
   );
 }
